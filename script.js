97758eabe1269b62141572e05c14bdf4c7323fb3
Roushan Sharma <87894817+Roushan-2104@users.noreply.github.com>
2021-09-07 21:50:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ let search_btn = document.getElementById("search-btn").addEventListener("click",
     e.preventDefault();
     let search = document.getElementById("search").value;
     image.innerHTML = "<em>LOADING....</EM>"
-    let download = "<i class='fas fa-download' id='download'></i>"
     fetch(`https://api.unsplash.com/search/photos?query=${search}&client_id=DGfGVXwxqqVycp_z7n31Qk5DxX1EbSgN6FjOni26A50&per_page=100`)
     .then(respose => respose.json())
     .then(data =>{
         setTimeout(() => {
             image.innerHTML="";
             data.results.forEach(photo => {
-            image.insertAdjacentHTML('afterbegin',`<div id="img"><img src="${photo.urls.small}" /><a href ="https://unsplash.com/photos/${photo.id}/download?force=true">${download}</a></div>`)
+            image.insertAdjacentHTML('afterbegin',`<div id="img"><img src="${photo.urls.small}" /><a href ="https://unsplash.com/photos/${photo.id}/download?force=true"><i class='fas fa-download download'></i></a></div>`)
             });
         }, 1000);
         
